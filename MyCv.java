831c29255de0c7d3496c1c79e2b4c17b1fad8150
Hawraa <hawra8516@gmail.com>
2025-06-30 11:14:26 +0400

    First commit

---
+++ MyCv.java
@@ -0,0 +1,36 @@
+public class MyCv {
+ public static void main(String [] args){
+  System.out.println("Hawraa Al-Rawahi");
+  System.out.println("Almudhaibi, Asharqiyah");
+  System.out.println("phone: +968 92230698");
+  System.out.println("Email: hawra8516@gmail.com");
+  System.out.println("Major: Networking & Database");
+  System.out.println("""
+Objective: 
+    To leverage my passion for technology and problem-
+    solving abilities in an IT role. As a highly motivated
+    and energetic professional, I strive to accomplish
+    tasks efficiently and confidently deliver results. I aim
+    to join a prestigious institution where I can apply my
+    skills and knowledge to drive innovation, improve
+    efficiency, and achieve outstanding results.
+""");
+  System.out.println("""
+  Skills: 
+    -Computer Proficiency 
+    -Data Analysis 
+    -Research skills
+    -Problem-Solving 
+    -Time Management  
+    -Communication
+  """);
+ System.out.println("""
+  Training Courses: 
+    -Participation in the Cybersecurity essentials for everyone workshop, 20th September 2022. 
+    -Participation in the training course in the Python program, 12th February 2023.
+    -Participation in the Mastering Prompt Engineering with ChatGPT: 
+	  How AI Personal Assistants Can Revolutionize Your Work workshop,9th March 2023.
+    -Practical training in Al-Mudhaibi Municipality, 14th May - 8th June 2023.
+  """);
+ }
+}
