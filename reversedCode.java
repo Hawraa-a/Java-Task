5012333ddbe36f75bca5f15360fbceb797e5181f
Hawraa <hawra8516@gmail.com>
2025-07-06 16:23:12 +0400

    Add ReversedCode

---
+++ reversedCode.java
@@ -0,0 +1,28 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String[] names = {"Nasser", "Haitham", "Manal", "Hanin"};
+        String[] rNames = reverNames(names);
+
+        System.out.print("{ ");
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.print(rNames[i]);
+
+            if (i < rNames.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("} ");
+    }
+    public static String[] reverNames(String[] names) {
+        String[] reversed = new String[names.length];
+        for (int i = 0; i < names.length; i++) {
+            reversed[i] = new StringBuilder(names[i]).reverse().toString();
+
+        }
+        return reversed;
+    }
+}
+
+
